eadd545ab3b4502b7582e8bd44bd6541c0a4aff7
erikhartker <erik.hartker@ufl.edu>
2022-11-06 20:14:19 -0500

    AVL Tree header updated with destructor

--- src/AVLTree.h
+++ src/AVLTree.h
@@ -3,11 +3,13 @@
 //
 #include <string>
 #include <iostream>
+#include <vector>
 using namespace std;
 
 #pragma once
 class AVLTree{
 public:
+    //struct of a node class for each individual node in the larger tree
     struct TreeNode{
         TreeNode* left = nullptr;
         TreeNode* right = nullptr;
@@ -17,18 +19,28 @@ public:
         int balanceFactor = 0;
         TreeNode(int val, string IDName, int nodeHeight) : value(val), name(IDName), height(nodeHeight), left(nullptr), right(nullptr), balanceFactor(0){}
     };
-    TreeNode* root = nullptr;
+
+    //the 9 public functions as well as the destructor
     TreeNode* insert(int ID, string IDName);
+    void searchName(string name);
     void remove(int ID);
-    void printInorder(TreeNode* node);
-    void printPreorder(TreeNode* node);
-    void printPostorder(TreeNode* node);
+    void printInorder();
+    void printPreorder();
+    void printPostorder();
     void printLevelCount();
     void removeInorder(int num);
     TreeNode* searchID(int ID);
+    ~AVLTree();
 
 private:
+    //all helper functions, as well as the root attribute
+    void destructorHelper(TreeNode* node);
+    void printPreorderHelper(TreeNode* node, vector<string>& names);
+    void printPostorderHelper(TreeNode* node, vector<string>& names);
+    void printInorderHelper(TreeNode* node, vector<string>& names);
+    TreeNode* root = nullptr;
     TreeNode* searchIDHelper(TreeNode* node, int ID);
+    void searchNameHelper(TreeNode* node, string name, vector<int>& idList);
     TreeNode* insertRecursive(TreeNode* root, int ID, string IDName, int height);
     TreeNode* leftRotation(TreeNode* root);
     TreeNode* rightRotation (TreeNode* root);
